e521c2f7388638f3eb7ce4364d5a373e7beb7a45
omarabbasi <omarkabbasi@outlook.com>
2022-02-14 10:55:28 -0800

    most current

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+// Global Variables
+url = 'https://raw.githubusercontent.com/muan/unicode-emoji-json/main/data-by-group.json'
+
+// DOM Selectors
 let buttn = document.getElementById('submit-butt')
 let box1 = document.getElementById('first-box')
 //let box2 = document.getElementById('second-box')
 let qrCode = document.getElementById("code-box")
 
+// Listeners
+
+
+
+// Fetchers
+function
+
+// Render Functions
+
+
+// Event Handlers
+
+
+// Initializer
