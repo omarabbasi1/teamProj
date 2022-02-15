791d63729960e7142b4aa32a1c511bec7b400d25
omarabbasi <omarkabbasi@outlook.com>
2022-02-14 16:44:52 -0800

    working version

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
-# teamProj
-QR Finder App
-Get requests can be made to the following API:
-https://qrickit.com/api/qr.php?d=yourdata
+FEELINGS FINDER - Your digital mood ring.
+
+Description:
+
+-Our app creates and outputs the wonderful tradition of mood translation. Much like the rings worn by children since the mid 70's - modern day.
+
+-Simply describe a feeling in the given text box, you will be promted once you 'mouseover' the box with suggestions, then click the 'Submit' button. 
+
+-The result will be a custom generated emoji to emulate your given mood.
+
+GETTING STARTED
+Installation Instructions:
+
+-Wow! it's 2022 and Feelings Finder requires no installation and run independently on your home browser for ease of use!
+
+VERSION HISTORY
+Current Version: 1.0 first gen.
+
+-AUTHORS-
+Beth Fekadu
+Omar Abbasi
+Olivia Pomeroy
 
--- index.js
+++ index.js
@@ -1,23 +1,62 @@
 // Global Variables
-url = 'https://raw.githubusercontent.com/muan/unicode-emoji-json/main/data-by-group.json'
+const url = 'https://type.fit/api/quotes'
 
 // DOM Selectors
 let buttn = document.getElementById('submit-butt')
 let box1 = document.getElementById('first-box')
-//let box2 = document.getElementById('second-box')
 let qrCode = document.getElementById("code-box")
+let quoteSpot = document.querySelector('#quoteSpot')
+let form = document.querySelector("#form")
+
 
 // Listeners
+qrCode.addEventListener("mouseover", function (){alert("Created by: Beth, Omar, Olivia")})
+form.addEventListener("submit", (renderOneQuote) => handleSubmit(e))
 
 
 
 // Fetchers
-function
+function fetchAllQuotes(quoteArr) {
+    return fetch(url)
+    .then (res => res.json())
+    // .then (console.log)
+}
 
 // Render Functions
 
+// function renderAllQuotes(quoteArr) {
+//     quoteArr.forEach(renderOneQuote)
+// }
 
+function renderOneQuote(quoteArr) {
+    let randomValue = quoteArr[Math.floor(Math.random() * quoteArr.length)]
+    let li = document.createElement('li')
+    li.textContent = randomValue.text
+
+    quoteSpot.appendChild(li)
+    // console.log(randomValue)
+
+    //form.addEventListener("submit", (e)=> handleSubmit(e))
+
+    // function handleSubmit(e) {
+    //     e.preventDefault()
+    //     console.log("testing submit")
+    //     renderOneQuote()
+    // }
+
+}
+
+
+
+
+    
 // Event Handlers
+function handleSubmit(e) {
+    e.preventDefault()
+    console.log("testing submit")
+    
+    renderOneQuote()
+}
 
-
-// Initializer
+// / Initializer
+fetchAllQuotes().then(renderOneQuote)
